182f1cf91508732a28fb896d6c65abf8daac56db
Leshnevskyi <leshnevskyi@gmail.com>
2023-11-26 01:56:57 +0200

    refactor(bst): explicit typing in tests

--- test/Lib/Data/BSTSpec.hs
+++ test/Lib/Data/BSTSpec.hs
@@ -7,21 +7,25 @@ spec :: Spec
 spec = do
   describe "singleton" $ do
     it "creates a BST with a single element" $ do
-      singleton 5 `shouldBe` Node 5 Nil Nil
+      singleton (5 :: Int) `shouldBe` Node 5 Nil Nil
 
   describe "insert" $ do
     it "inserts an element into an empty BST" $ do
-      insert 5 Nil `shouldBe` Node 5 Nil Nil
+      insert (5 :: Int) Nil `shouldBe` Node 5 Nil Nil
 
     it "inserts an element into a non-empty BST" $ do
-      let bst = singleton 5
+      let bst = singleton (5 :: Int)
           bst' = insert 3 bst
       bst' `shouldBe` Node 5 (Node 3 Nil Nil) Nil
 
   describe "fromList" $ do
-    it "creates a BST from a list of elements" $ do
-      fromList [5, 3, 7] `shouldBe` Node 5 (Node 3 Nil Nil) (Node 7 Nil Nil)
+    it "creates a BST from a list of elements" $
+      do
+        fromList ([5, 3, 7] :: [Int])
+        `shouldBe` Node 5 (Node 3 Nil Nil) (Node 7 Nil Nil)
 
   describe "findByLevel" $ do
-    it "returns a list of elements at a specific level in the BST" $ do
-      findByLevel 1 (Node 5 (Node 3 (Node 2 Nil Nil) Nil) (Node 7 Nil Nil)) `shouldBe` [3, 7]
+    it "returns a list of elements at a specific level in the BST" $
+      do
+        findByLevel 1 $ fromList ([5, 3, 2, 7] :: [Int])
+        `shouldBe` [3, 7]

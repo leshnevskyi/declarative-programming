26007d09139263bcc08deb15260f11da8d6c23ba
Leshnevskyi <leshnevskyi@gmail.com>
2023-11-25 20:12:33 +0200

    test(list): basic test cases

---
+++ test/Lib/Data/ListSpec.hs
@@ -0,0 +1,20 @@
+module Lib.Data.ListSpec (spec) where
+
+import qualified Lib.Data.List as List
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Lib.Data.List" $ do
+    describe "product" $ do
+      it "returns identity for empty list" $ do
+        List.product [] `shouldBe` (1 :: Int)
+
+      it "returns the product of all items in a list" $ do
+        List.product [0 .. 10] `shouldBe` (0 :: Int)
+        List.product [1 .. 5] `shouldBe` (120 :: Int)
+
+    describe "product implementations" $ do
+      it "are equivalent" $ do
+        List.product ([1 .. 10] :: [Int]) `shouldBe` List.product' [1 .. 10]
+        List.product ([1 .. 10] :: [Int]) `shouldBe` List.product'' [1 .. 10]

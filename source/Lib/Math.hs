fb04a2b9456da533971988f850b1aaf00e20335c
Leshnevskyi <leshnevskyi@gmail.com>
2023-12-05 17:07:26 +0200

    feat(math): infinite prime numbers list

--- source/Lib/Math.hs
+++ source/Lib/Math.hs
@@ -1,4 +1,10 @@
-module Lib.Math (fibs) where
+module Lib.Math (fibs, primes) where
 
 fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+primes :: [Integer]
+primes = sieve [2 ..]
+  where
+    sieve [] = []
+    sieve (p : xs) = p : sieve [x | x <- xs, x `mod` p /= 0]

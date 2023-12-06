603de4d6d2c5a9aa958a4f4cd3b0f8ee863b5ec0
Leshnevskyi <leshnevskyi@gmail.com>
2023-12-06 14:11:19 +0200

    task(streams): stream handling examples

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,44 @@
 module Main (main) where
 
-import Lib.Utils (isValidPercentage, stringMul)
+import Conduit
+import Data.List (intersect, nub, sort, union, (\\))
+import Lib.Data.List
+  ( reversedQuickSort,
+    uselessList10,
+    uselessList10',
+    uselessList10C,
+    uselessList5,
+    uselessList5',
+    uselessList5C,
+  )
+import Lib.Math (primes)
+
+firstList :: [Integer]
+firstList = take 45 uselessList5
+
+firstList' :: [Integer]
+firstList' = take 45 uselessList5'
+
+secondList :: [Integer]
+secondList = take 60 uselessList10
+
+secondList' :: [Integer]
+secondList' = take 60 uselessList10'
 
 main :: IO ()
 main = do
-  putStrLn $ stringMul (2, 5)
-  print (isValidPercentage 50)
+  print $ take 1000 primes
+
+  print $ reversedQuickSort firstList
+  print $ sort firstList
+  print $ nub firstList
+  print $ firstList == firstList'
+  print =<< runConduit (uselessList5C .| takeC 45 .| sinkList)
+
+  print $ reversedQuickSort secondList
+  print $ secondList == secondList'
+
+  print $ firstList `union` secondList
+  print $ firstList `intersect` secondList
+  print $ firstList \\ secondList
+  print =<< runConduit (uselessList10C .| takeC 60 .| sinkList)
--- source/Lib/Data/List.hs
+++ source/Lib/Data/List.hs
@@ -3,8 +3,18 @@ module Lib.Data.List
     product',
     product'',
     quickSort,
+    reversedQuickSort,
+    uselessList5,
+    uselessList5',
+    uselessList5C,
+    uselessList10,
+    uselessList10',
+    uselessList10C,
   )
 where
+
+import Conduit
+import Data.Function ((&))
 
 product :: (Num a) => [a] -> a
 product = foldr (*) 1
@@ -23,3 +33,29 @@ quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
 quickSort (x : xs) =
   quickSort (filter (< x) xs) ++ [x] ++ quickSort (filter (>= x) xs)
+
+reversedQuickSort :: (Ord a) => [a] -> [a]
+reversedQuickSort = reverse . quickSort
+
+endsWithDigit :: Integer -> Integer -> Bool
+endsWithDigit number = (number `mod` 10 ==)
+
+-- The task suggests to check if a number ends with 3, but the program hangs
+-- because there are no numbers that end with 3 in the list.
+uselessList5 :: [Integer]
+uselessList5 = [1 ..] & filter (`endsWithDigit` 6) . map (^ 4)
+
+uselessList5' :: [Integer]
+uselessList5' = [x ^ 4 | x <- [1 ..], (x ^ 4) `endsWithDigit` 6]
+
+uselessList5C :: (Monad m) => ConduitT () Integer m ()
+uselessList5C = yieldMany [1 ..] .| mapC (^ 4) .| filterC (`endsWithDigit` 6)
+
+uselessList10 :: [Integer]
+uselessList10 = filter (== 3) $ map (`mod` 5) [1 ..]
+
+uselessList10' :: [Integer]
+uselessList10' = [x `mod` 5 | x <- [1 ..], x `mod` 5 == 3]
+
+uselessList10C :: (Monad m) => ConduitT () Integer m ()
+uselessList10C = yieldMany [1 ..] .| mapC (`mod` 5) .| filterC (== 3)
